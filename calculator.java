0c1d2751c0dc6158dd6311d084e90d88f8ae5dcc
mimi-daniella <mmesomaalagwu@gmail.com>
2024-11-01 12:06:49 +0100

    Added additional method to handle decimal number in calculator class

--- calculator.java
+++ calculator.java
@@ -1,7 +1,11 @@
 public class calculator{
         public int addition(int a ,int b){
 		return a + b;
-     }
+        }
+
+	public double addition(int a, double b){
+               return a+b;
+	}
 
 }
 
